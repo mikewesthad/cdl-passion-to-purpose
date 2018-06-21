d46e7c80ae404b30d09dc0652117dab62ef831b8
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-20 22:10:17 -0500

    Reorg game store into two separate stores (that are instances of ResponsesStore)

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,96 @@
-import { observable, action } from "mobx";
+import { extendObservable, observable, action, computed, autorun } from "mobx";
 import syncToStorage from "./sync-to-storage";
 
-class GameStore {
-  @observable passions = [];
-  @observable purposes = [];
+const numQuestions = 6;
+const emptyStringArray = length => new Array(length).fill("");
+const isValid = elem => elem !== "";
+class GameData {
+  constructor() {
+    this.passionStore = new ResponsesStore(this, [
+      "I spend my time",
+      "I am really good at",
+      "I talk to my friends about",
+      "I spend my money on",
+      "I am a fan of",
+      "I read all about"
+    ]);
 
-  @action
-  reset = () => {
-    this.passions = [];
-    this.purposes = [];
-  };
+    this.purposeStore = new ResponsesStore(this, [
+      "Challenge",
+      "Raise awareness about",
+      "Protest",
+      "Improve",
+      "Advocate for",
+      "Change"
+    ]);
+  }
 
-  @action
-  setPassions = passions => {
-    this.passions = passions;
-  };
-  @action
-  setPurposes = purposes => {
-    this.purposes = purposes;
-  };
+  reset() {
+    this.passionStore.reset();
+    this.purposeStore.reset();
+  }
 
-  // Controlled serialization
+  // Controlled Serialization
   toJSON() {
     return {
-      passions: this.passions,
-      purposes: this.purposes
+      passions: this.passionStore.toJSON(),
+      purposes: this.purposeStore.toJSON()
     };
+  }
+  fromJSON(json) {
+    if (json.passions) this.passionStore.fromJSON(json.passions);
+    if (json.purposes) this.purposeStore.fromJSON(json.purposes);
   }
 }
 
-const store = new GameStore();
+class ResponsesStore {
+  constructor(rootStore, questions) {
+    this.rootStore = rootStore;
+    this.questions = questions;
+    this.numQuestions = questions.length;
+
+    extendObservable(this, {
+      responses: emptyStringArray(this.numQuestions)
+    });
+  }
+
+  getQuestions() {
+    return this.questions;
+  }
+  isResponseValid(i) {
+    return isValid(this.responses[i]);
+  }
+  areAllResponsesValid() {
+    return this.responses.every(isValid);
+  }
+  toJSON() {
+    return this.responses.slice();
+  }
+  fromJSON(json) {
+    this.setResponses(json);
+  }
+
+  @action
+  setResponse = (i, response) => {
+    this.responses[i] = response;
+  };
+  @action
+  setResponses = responses => {
+    this.responses = responses;
+  };
+  @action
+  reset() {
+    this.responses = emptyStringArray(this.numQuestions);
+  }
+}
+
+// const store = new GameStore();
+const store = new GameData();
+
+autorun(() => {
+  console.log(store.passionStore.responses.slice());
+  console.log(store.purposeStore.responses.slice());
+});
 
 syncToStorage("passion-to-purpose-data", store);
 
--- src/store/sync-to-storage.js
+++ src/store/sync-to-storage.js
@@ -4,12 +4,8 @@ import store from "store";
 export default function autoSync(key, gameStore) {
   // First run - initialize storage or load values from storage into gameStore
   const storedData = store.get(key);
-  if (storedData === undefined) store.set(key, gameStore);
-  else {
-    for (const [key, value] of Object.entries(storedData)) {
-      gameStore[key] = value;
-    }
-  }
+  if (storedData === undefined) store.set(key, gameStore.toJSON());
+  else gameStore.fromJSON(storedData);
 
   // Reactively sync to local storage
   autorun(() => store.set(key, gameStore));
