18631053d56d3b97100e528a3fbba6ee5766dc8f
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-20 22:11:23 -0500

    Home page lingo

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,9 +4,13 @@ import { Link } from "react-router-dom";
 export default function Home(props) {
   return (
     <div>
-      <div>Home</div>
+      <h1>Passion to Purpose</h1>
       <div>
-        <Link to={props.nextRoute}>Next</Link>
+        <p>How can you turn your passion into a purpose?</p>
+        <p>Answer a couple simple questions to generate design questions to inspire yourself.</p>
+      </div>
+      <div>
+        <Link to={props.nextRoute}>Let's Go ➞</Link>
       </div>
     </div>
   );
