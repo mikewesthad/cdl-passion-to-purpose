5ee785c62f821ea8a910d5873adda980cdee72b2
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-22 07:21:42 -0500

    Import montserrat typeface

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import "normalize.css";
 import "./style/global/index.scss";
+import "typeface-montserrat";
 // import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
