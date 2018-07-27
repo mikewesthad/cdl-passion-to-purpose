b79c0e356073b1f3956b3fd6894428f304ffe83e
Michael Hadley <michaelwesthadley@gmail.com>
2018-07-27 09:54:20 -0500

    New fb setup via env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,11 +2,13 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const config = {
-  apiKey: "XXXXXXXXX",
-  authDomain: "passion-to-purpose.firebaseapp.com",
-  databaseURL: "https://passion-to-purpose.firebaseio.com",
-  projectId: "passion-to-purpose",
-  storageBucket: "",
-  messagingSenderId: "975548954395"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PRODJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID
 };
-export default firebase.initializeApp(config);
+firebase.initializeApp(config);
+
+export default firebase;
