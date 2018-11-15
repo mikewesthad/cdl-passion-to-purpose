c3aa2e94fcd61da0f00cc4bec86e529a7d88bdbd
Michael Hadley <michaelwesthadley@gmail.com>
2018-11-14 21:08:29 -0600

    Restructure app to split routing from main app

--- src/app.js
+++ src/app.js
@@ -1,74 +1,79 @@
-import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import React, { PureComponent, Component } from "react";
+import { MemoryRouter, Route, Switch, Redirect, withRouter } from "react-router-dom";
 import { observer } from "mobx-react";
+import Url from "url-parse";
 import PageTransition from "./components/page-transition";
 import PageWrapper from "./components/page-wrapper";
 import Analytics from "./components/analytics";
 import { routes, routeMap } from "./pages";
 import gameData from "./store";
+import Nav from "./components/nav";
 
-// create-react-app uses package.json's homepage field to configure the path for assets, so use the
-// same URL to figure out the basename for the router
 const isDev = process.env.NODE_ENV === "development";
-let basename = "";
-if (!isDev) {
-  const publicUrl = process.env.PUBLIC_URL;
-  const parts = publicUrl.replace(/https?:\/\//, "").split("/");
-  basename = parts.slice(1).join("/");
-}
 
-class App extends React.Component {
+// TODO: verify this works in production and add to GA
+const baseUrl = new Url(isDev ? "http://localhost:3000" : process.env.PUBLIC_URL);
+const currentUrl = new Url(window.location);
+const gameRoom = currentUrl.pathname.slice().replace(baseUrl.pathname, "");
+console.log(`Game Room: ${gameRoom}`);
+
+const App = withRouter(
+  class App extends Component {
+    goBack = () => this.props.history.goBack();
+    restart = () => this.props.history.push(routeMap.home.path);
+
+    render() {
+      return (
+        <PageWrapper>
+          <Nav onBack={this.goBack} onRestart={this.restart} />
+
+          <Analytics
+            dummyLog={isDev}
+            trackingId="UA-114340105-6"
+            gameStartRoute={routeMap.home.path}
+            gameEndRoute={routeMap.generator.path}
+          />
+
+          <Route
+            render={({ location }) => (
+              <PageTransition pageKey={location.pathname}>
+                <Switch location={location}>
+                  {routes.map((route, i) => {
+                    const nextRoute = i < routes.length - 1 ? routes[i + 1].path : routes[0].path;
+
+                    // All pages have the same general API - they need the game store & next route
+                    const { key, path, Component, ...otherProps } = route;
+                    return (
+                      <Route
+                        key={key}
+                        path={path}
+                        {...otherProps}
+                        render={props => (
+                          <Component gameData={gameData} nextRoute={nextRoute} {...props} />
+                        )}
+                      />
+                    );
+                  })}
+
+                  <Redirect to={routeMap.home.path} />
+                </Switch>
+              </PageTransition>
+            )}
+          />
+        </PageWrapper>
+      );
+    }
+  }
+);
+
+class RoutedApp extends PureComponent {
   render() {
     return (
-      <BrowserRouter basename={basename}>
-        <Route
-          render={({ location }) => {
-
-            return (
-              <PageWrapper>
-                <Analytics
-                  basename={basename}
-                  dummyLog={isDev}
-                  trackingId="UA-114340105-6"
-                  gameStartRoute={routeMap.home.path}
-                  gameEndRoute={routeMap.generator.path}
-                />
-
-                <Route
-                  render={({ location }) => (
-                    <PageTransition pageKey={location.pathname}>
-                      <Switch location={location}>
-                        {routes.map((route, i) => {
-                          const nextRoute =
-                            i < routes.length - 1 ? routes[i + 1].path : routes[0].path;
-
-                          // All pages have the same general API - they need the game store, next
-                          // route and previous route
-                          const { key, path, Component, ...otherProps } = route;
-                          return (
-                            <Route
-                              key={key}
-                              path={path}
-                              {...otherProps}
-                              render={props => (
-                                <Component gameData={gameData} nextRoute={nextRoute} {...props} />
-                              )}
-                            />
-                          );
-                        })}
-
-                        <Redirect to={routeMap.home.path} />
-                      </Switch>
-                    </PageTransition>
-                  )}
-                />
-              </PageWrapper>
-            );
-          }}
-        />
-      </BrowserRouter>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
     );
   }
 }
 
-export default observer(App);
+export default observer(RoutedApp);
