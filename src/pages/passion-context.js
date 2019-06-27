cd29c10d68cb1a6ca0dc636190e3c4239c62bfbb
hhoare <henry.hoare@hotmail.com>
2019-06-27 13:56:49 -0400

    Fill out temp passion context page

--- src/pages/passion-context.js
+++ src/pages/passion-context.js
@@ -6,10 +6,10 @@ export default function PassionContext(props) {
   return (
     <Container>
       <div className="context-container" to={props.nextRoute}>
-        <h2 className="title">Quick tip!</h2>
+        <h2 className="title">Passion</h2>
         <div className="description">
-          <p>Don't do this</p>
-          <p>Do this</p>
+          <p>First, we're going to ask you about your passions.</p>
+          <p>Answer honestly, and only provide one answer to each question.</p>
         </div>
       </div>
       <div className="text-center">
