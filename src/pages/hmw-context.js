063bae9b5ecb74c77994364ae6e8a3026a0f888a
hhoare <henry.hoare@hotmail.com>
2019-06-27 14:02:26 -0400

    Filled out placeholder HMW context page language

--- src/pages/hmw-context.js
+++ src/pages/hmw-context.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Container from "../components/container";
 
-export default function PurposeContext(props) {
+export default function HMWContext(props) {
   return (
     <Container>
       <div className="context-container" to={props.nextRoute}>
-        <h2 className="title">Quick tip!</h2>
+        <h2 className="title">How might we...?</h2>
         <div className="description">
-          <p>Don't do this</p>
-          <p>Do this</p>
+          <p>Now, we're going to combine your answers</p>
+          <p>Click to see the different combinations and choose your favorite!</p>
         </div>
       </div>
       <div className="text-center">
