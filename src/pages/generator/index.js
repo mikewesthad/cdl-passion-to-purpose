16d81a558d0a5a4e92cac9e0029203cc146da629
Lorelei <lorelei.miyamura@gmail.com>
2019-07-03 14:28:23 -1000

    Adding navigation to the last page of P2P module

--- src/pages/generator/index.js
+++ src/pages/generator/index.js
@@ -53,10 +53,7 @@ class Generator extends React.Component {
         <div className={style.generatedQuestion}>
           <span className={style.hmwPreset}>How might we use</span>
           <div className={style.passionContainer}>
-            <GenerateAttribution
-              style={{ display: "inline", height: "100%", margin: "auto" }}
-              onClick={this.getNextPassion}
-            />
+            <GenerateAttribution style={{ display: "inline" }} onClick={this.getNextPassion} />
             <span className={style.generatedPassion}>{passion}</span>
           </div>
           <span className={style.hmwPreset}>to</span>
@@ -75,6 +72,9 @@ class Generator extends React.Component {
             Restart
           </Link>
     </div>*/}
+        <div className="navigation">
+          <input className="button submit-button" type="submit" value="Continue with this ➞" />
+        </div>
         <div className="social-container">
           <SocialShare passion={passion} purpose={purpose} />
         </div>
