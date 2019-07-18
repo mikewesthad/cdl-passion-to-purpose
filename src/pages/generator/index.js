5a397f834408bc50adf3e5043b850b8fae40d670
Lorelei <lorelei.miyamura@gmail.com>
2019-07-18 09:40:39 -1000

    Placing shuffle button within a styling inline container

--- src/pages/generator/index.js
+++ src/pages/generator/index.js
@@ -60,10 +60,12 @@ class Generator extends React.Component {
         <div className={style.generatedQuestion}>
           <span className={style.hmwPreset}>How might we use</span>
           <div className={style.passionContainer}>
+            <GenerateAttribution className={style.generatedButton} onClick={this.getNextPassion} />
             <span className={style.generatedPassion}>{passion}</span>
           </div>
           <span className={style.hmwPreset}>to</span>
           <div className={style.purposeContainer}>
+            <GenerateAttribution className={style.generatedButton} onClick={this.getNextPurpose} />
             <span className={style.generatedPurpose}>{purpose}?</span>
           </div>
         </div>
