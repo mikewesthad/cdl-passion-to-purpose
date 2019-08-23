4b2284c68fe2c7a25242d06292eb2c71967827df
henryhoare <henry.hoare@hotmail.com>
2019-08-23 16:57:49 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0abf25c581c8bcf0d618275f7fd69874.js"
+  "./precache-manifest.2e19b6134562d76e23eac0bc42f35c4c.js"
 );
 
 workbox.clientsClaim();
