cf52568d0e9d768bcfe8e5d4c09c2f6c297ed6b6
Lorelei <lorelei.miyamura@gmail.com>
2019-09-22 17:19:32 -0500

    Laying out hidden toggle for generator page

--- src/pages/generator/index.js
+++ src/pages/generator/index.js
@@ -8,10 +8,15 @@ import style from "./index.module.scss";
 class Generator extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { isHidden: true };
 
     const gameData = this.props.gameData;
     gameData.shufflePassions();
     gameData.shufflePurposes();
+  }
+
+  toggleHidden() {
+    this.setState({ isHidden: true });
   }
 
   storeP2P = () => {
@@ -23,6 +28,21 @@ class Generator extends React.Component {
     const { gameData, nextRoute } = this.props;
     const passion = gameData.passionStore.responses[gameData.chosenPassionIndex];
     const purpose = gameData.getPurposesWithVerb()[gameData.chosenPurposeIndex];
+    const Child = () => <span className={style.generatedPassion}>{passion}</span>;
+    const Parent = () => (
+      <div className={style.overflowContainer}>
+        <div className={style.animation}>
+          <span className={style.generatedPassion}>
+            <ul>
+              <li>{gameData.passionStore.responses[0]}</li>
+              <li>{gameData.passionStore.responses[1]}</li>
+              <li>{gameData.passionStore.responses[2]}</li>
+              <li>{gameData.passionStore.responses[3]}</li>
+            </ul>
+          </span>
+        </div>
+      </div>
+    );
 
     return (
       <GeneratorTemplate>
@@ -31,9 +51,14 @@ class Generator extends React.Component {
           <div className={style.passionContainer}>
             <GenerateAttribution
               className={style.generatedButton}
-              onClick={gameData.incrementPassionIndex}
+              //onClick={gameData.incrementPassionIndex}
+              onClick={this.toggleHidden.bind(this)}
             />
-            <span className={style.generatedPassion}>{passion}</span>
+            {!this.state.isHidden && <Child />}
+            {this.state.isHidden && <Parent />}
+            <div className={style.hidden}>
+              <span className={style.generatedPassion}>{passion}</span>
+            </div>
           </div>
           <span className={style.hmwPreset}>to</span>
           <div className={style.purposeContainer}>
@@ -41,7 +66,9 @@ class Generator extends React.Component {
               className={style.generatedButton}
               onClick={gameData.incrementPurposeIndex}
             />
-            <span className={style.generatedPurpose}>{purpose}?</span>
+            <div className={style.hidden}>
+              <span className={style.generatedPurpose}>{purpose}?</span>
+            </div>
           </div>
         </div>
         <div className="generateButtonContainer" style={{ textAlign: "center" }}>
