0ce0ce8767a73b63f9c84e87aa19a461806505ff
Lorelei <lorelei.miyamura@gmail.com>
2019-09-22 18:52:41 -0500

    Cleanup of Medium's context page

--- src/pages/medium-context.js
+++ src/pages/medium-context.js
@@ -3,20 +3,19 @@ import { Link } from "react-router-dom";
 import Container from "../components/container";
 
 export default function MediumContext(props) {
-    return (
-      <Container>
-        <div className="context-container" to={props.nextRoute}>
-          <h2 className="title">Explore Potential Mediums</h2>
-          <div className="description">
-            <p>How might you refine your question to make it more specific and inspiring?</p>
-          </div>
+  return (
+    <Container>
+      <div className="context-container" to={props.nextRoute}>
+        <h2 className="title">Explore Potential Mediums</h2>
+        <div className="description">
+          <p>How might you refine your question to make it more specific and inspiring?</p>
         </div>
-        <div className="text-center">
-          <Link className="button" to={props.nextRoute}>
-            Let's explore! ➞
-          </Link>
-        </div>
-      </Container>
-    );
-  }
-  
+      </div>
+      <div className="text-center">
+        <Link className="button" to={props.nextRoute}>
+          Let's explore! ➞
+        </Link>
+      </div>
+    </Container>
+  );
+}
