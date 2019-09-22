529f8a838f42739b6a0e98c5c3fd02e1b7ad8cad
Lorelei <lorelei.miyamura@gmail.com>
2019-09-22 14:44:12 -0500

    Removal of unused variables on the Medium page

--- src/pages/medium.js
+++ src/pages/medium.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { observer, inject } from "mobx-react";
 import MediumPromptTemplate from "../components/medium-prompt-template";
-import generator from "./generator";
 //import { reaction } from "mobx";
 
 class Medium extends React.Component {
@@ -23,27 +22,6 @@ class Medium extends React.Component {
             <p>Examples:</p>
           </React.Fragment>
         }
-        description2={
-          <React.Fragment>
-            <div>
-              <ul class="columns" data-columns="2">
-                <li>Poster</li>
-                <li>Community Event</li>
-                <li>Board Game</li>
-                <li>Movie</li>
-                <li>Podcast</li>
-                <li>Website</li>
-                <li>Sculpture</li>
-              </ul>
-            </div>
-          </React.Fragment>
-        }
-        medium1={
-          <React.Fragment>
-            <p>I will make a...</p>
-          </React.Fragment>
-        }
-        stepNumber="5"
       />
     );
   }
