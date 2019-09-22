1b550c2ec622ec079340d8973c5939cef0061faf
henryhoare <henry.hoare@hotmail.com>
2019-09-22 15:03:33 -0500

    Removes usage of react-animations and styled-components

--- src/pages/hmw-context.js
+++ src/pages/hmw-context.js
@@ -1,22 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Container from "../components/container";
-import styled, { keyframes } from "styled-components";
-import { bounce, merge, fadeInDown, fadeOutDown } from "react-animations";
-const fadeInOut = merge(fadeInDown, fadeOutDown);
-const bounceAnimation = keyframes`${fadeInOut}`;
-
-const BouncyDiv = styled.div`
-  animation: 2s linear infinite ${bounceAnimation};
-`;
 
 export default function HMWContext(props) {
   return (
     <Container>
       <div className="context-container" to={props.nextRoute}>
-        <BouncyDiv>
-          <h2 className="title">How might we...?</h2>
-        </BouncyDiv>
+        <h2 className="title">How might we...?</h2>
         <div className="description">
           <p>Let's combine your responses!</p>
           <p>Click to see the different combinations and choose your favorite.</p>
