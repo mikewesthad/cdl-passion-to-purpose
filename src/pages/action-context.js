9461c34b1aaa2093a91c4c2904acf8518863c24c
henryhoare <henry.hoare@hotmail.com>
2019-09-24 00:41:26 -0500

    Create and fill action context page

---
+++ src/pages/action-context.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "../components/container";
+
+export default function ActionContext(props) {
+  return (
+    <Container>
+      <div className="context-container" to={props.nextRoute}>
+        <h2 className="title">Explore Potential Affordances</h2>
+        <div className="description">
+          <p>
+            Which affordance might best represent your goals in answering your How Might We
+            question?
+          </p>
+        </div>
+      </div>
+      <div className="text-center">
+        <Link className="button" to={props.nextRoute}>
+          Let's explore! ➞
+        </Link>
+      </div>
+    </Container>
+  );
+}
