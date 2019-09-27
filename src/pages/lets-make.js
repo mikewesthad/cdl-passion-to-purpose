3b82e152b5b6d86392494e94ecffefd0a55853c3
Lorelei <lorelei.miyamura@gmail.com>
2019-09-26 20:50:24 -0500

    Styling for Let's Make & HMW page

--- src/pages/lets-make.js
+++ src/pages/lets-make.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Container from "../components/container";
+import style from "./make-generator/index.module.scss";
 
 export default function LetsMake(props) {
   return (
     <Container>
-      <div className="context-container" to={props.nextRoute}>
-        <h2 className="title">Let's Make Page</h2>
-        <div className="description">
-          <p>There will be stuff here!</p>
-          <p>Your HMW and LM will go here!</p>
-        </div>
+      <div className={style.generatedQuestion}>
+        <span className={style.bolded}>How might we use </span>
+        <span className={style.generatedPassion}>passion</span>
+        <span className={style.bolded}> to </span>
+        <span className={style.generatedPurpose}>purpose?</span>
       </div>
+      <div className={style.generatedQuestion}>
+        <span className={style.bolded}>Let's make a </span>
+        <span className={style.generatedPassion}>medium</span>
+        <span className={style.bolded}> to </span>
+        <span className={style.generatedPurpose}>impact.</span>
+      </div>
+
       <div className="text-center">
         <Link className="button" to={props.nextRoute}>
           Continue ➞
