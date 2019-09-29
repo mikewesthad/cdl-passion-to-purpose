ed3538ae61de09d737db2ddb22ff386061a8b62b
Lorelei <lorelei.miyamura@gmail.com>
2019-09-29 16:19:35 -0500

    Adding a render function to the Medium Cards page

--- src/pages/medium-cards/index.js
+++ src/pages/medium-cards/index.js
@@ -1,38 +1,44 @@
 import React from "react";
+import { observer, inject } from "mobx-react";
 import { Link } from "react-router-dom";
 import CardsContainer from "../../components/medium-cards-template";
 import style from "./index.module.scss";
 import GenerateAttribution from "../../components/generator-attribution";
 
-export default function MediumCards(props) {
-  return (
-    <CardsContainer>
-      <div className={style.generatedPrompt}>
-        <span className={style.generatedPrompt}>Let's make a </span>
-        <span className={style.selectedMedium}>medium!</span>
-      </div>
-      <div className={style.cardsContainer}>
-        <div className={style.exampleCard}>
-          <div className={style.tags}>Tags</div>
-          <div className={style.mediaTitle}>Medium</div>
-          <div className={style.link}>Link</div>
+class MediumCards extends React.Component {
+  render() {
+    const { gameData } = this.props;
+    const medium = gameData.medium;
+    return (
+      <CardsContainer>
+        <div className={style.generatedPrompt}>
+          <span className={style.generatedPrompt}>Let's make a </span>
+          <span className={style.generatedPrompt}>{medium}!</span>
         </div>
-        <div className={style.affordanceCard}>
-          <div className={style.affordanceTitle}>Affordance</div>
+        <div className={style.cardsContainer}>
+          <div className={style.exampleCard}>
+            <div className={style.tags}>Tags</div>
+            <div className={style.mediaTitle}>{medium}</div>
+            <div className={style.link}>Link</div>
+          </div>
+          <div className={style.affordanceCard}>
+            <div className={style.affordanceTitle}>Affordance</div>
+          </div>
+          <div className={style.descriptionCard}>
+            <div className={style.projectDescription}>Project Description</div>
+          </div>
         </div>
-        <div className={style.descriptionCard}>
-          <div className={style.projectDescription}>Project Description</div>
+        <div className="text-center">
+          <GenerateAttribution
+            className={style.generatedButton}
+            //onClick={this.toggleHiddenPassion.bind(this)}
+          />
+          <Link className="button" to={this.props.nextRoute}>
+            Continue ➞
+          </Link>
         </div>
-      </div>
-      <div className="text-center">
-        <GenerateAttribution
-          className={style.generatedButton}
-          //onClick={this.toggleHiddenPassion.bind(this)}
-        />
-        <Link className="button" to={props.nextRoute}>
-          Continue ➞
-        </Link>
-      </div>
-    </CardsContainer>
-  );
+      </CardsContainer>
+    );
+  }
 }
+export default inject("gameData")(observer(MediumCards));
