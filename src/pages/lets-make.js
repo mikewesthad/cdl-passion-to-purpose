9eeac249d64ea786d7cbd2d93c72578d6cde9714
henryhoare <henry.hoare@hotmail.com>
2019-09-29 13:31:51 -0500

    Switches the LetsMake page to be a class that extends React.Component, giving it access to gameData

--- src/pages/lets-make.js
+++ src/pages/lets-make.js
@@ -1,29 +1,39 @@
 import React from "react";
+import { observer, inject } from "mobx-react";
 import { Link } from "react-router-dom";
 import Container from "../components/container";
 import style from "./make-generator/index.module.scss";
 
-export default function LetsMake(props) {
-  return (
-    <Container>
-      <div className={style.generatedQuestion}>
-        <span className={style.bolded}>How might we use </span>
-        <span className={style.generatedPassion}>passion</span>
-        <span className={style.bolded}> to </span>
-        <span className={style.generatedPurpose}>purpose?</span>
-      </div>
-      <div className={style.generatedQuestion}>
-        <span className={style.bolded}>Let's make a </span>
-        <span className={style.generatedPassion}>medium</span>
-        <span className={style.bolded}> to </span>
-        <span className={style.generatedPurpose}>impact.</span>
-      </div>
+class LetsMake extends React.Component {
+  render() {
+    const { gameData } = this.props;
 
-      <div className="text-center">
-        <Link className="button" to={props.nextRoute}>
-          Continue ➞
-        </Link>
-      </div>
-    </Container>
-  );
+    const passion = gameData.passionStore.responses[gameData.chosenPassionIndex];
+    const purpose = gameData.getPurposesWithVerb()[gameData.chosenPurposeIndex];
+    const medium = gameData.medium;
+    const impact = gameData.impact;
+    return (
+      <Container>
+        <div className={style.generatedQuestion}>
+          <span className={style.bolded}>How might we use </span>
+          <span className={style.generatedPassion}>{passion}</span>
+          <span className={style.bolded}> to </span>
+          <span className={style.generatedPurpose}>{purpose}?</span>
+        </div>
+        <div className={style.generatedQuestion}>
+          <span className={style.bolded}>Let's make a </span>
+          <span className={style.generatedPassion}>{medium}</span>
+          <span className={style.bolded}> to </span>
+          <span className={style.generatedPurpose}>{impact}.</span>
+        </div>
+
+        <div className="text-center">
+          <Link className="button" to={this.props.nextRoute}>
+            Continue ➞
+          </Link>
+        </div>
+      </Container>
+    );
+  }
 }
+export default inject("gameData")(observer(LetsMake));
