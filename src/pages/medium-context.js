a4975ec2984ed159c6bc2b367cceb9df34fa4e20
Lorelei <lorelei.miyamura@gmail.com>
2019-09-30 11:38:47 -0500

    Media Context update for CLS

--- src/pages/medium-context.js
+++ src/pages/medium-context.js
@@ -6,9 +6,9 @@ export default function MediumContext(props) {
   return (
     <Container>
       <div className="context-container" to={props.nextRoute}>
-        <h2 className="title">Explore Potential Mediums</h2>
+        <h2 className="title">Let's explore possible media to jumpstart your ideation process!</h2>
         <div className="description">
-          <p>What medium might best serve you as a way to answer your How Might We question?</p>
+          <p>Which media might help answer your "How Might We" question?</p>
         </div>
       </div>
       <div className="text-center">
