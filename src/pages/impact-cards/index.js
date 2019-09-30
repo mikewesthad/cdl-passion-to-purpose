1b780c2669692cbc1b516c81534b9ff1de1a6de7
Lorelei <lorelei.miyamura@gmail.com>
2019-09-30 18:36:54 -0500

    Impact card in progress fix

--- src/pages/impact-cards/index.js
+++ src/pages/impact-cards/index.js
@@ -1,46 +1,41 @@
 import React from "react";
 import { observer, inject } from "mobx-react";
 import { Link } from "react-router-dom";
-import CardsContainer from "../../components/medium-cards-template";
+import Container from "../../components/medium-cards-template";
 import style from "./index.module.scss";
 import GenerateAttribution from "../../components/generator-attribution";
 
 class ImpactCards extends React.Component {
   render() {
-    const { gameData } = this.props;
+    const { gameData, nextRoute } = this.props;
     const medium = gameData.medium;
     const impact = gameData.impact;
     return (
-      <CardsContainer>
-        <div className={style.generatedPrompt}>
-          <span className={style.generatedPrompt}>Let's make a </span>
-          <span className={style.generatedPrompt}>{medium}</span>
-          <span> to</span>
-          <span className={style.selectedImpact}> {impact}!</span>
+      <Container>
+        <h1 className="title">
+          Let's make a {medium} to {impact}!
+        </h1>
+        <div className="description">
+          Cycle through the {impact} category to view various types of projects done similarly in
+          your community!
         </div>
+
         <div className={style.cardsContainer}>
-          <div className={style.exampleCard}>
-            <div className={style.tags}>Tags</div>
-            <div className={style.mediaTitle}>Impact</div>
-            <div className={style.link}>Link</div>
-          </div>
-          <div className={style.affordanceCard}>
-            <div className={style.affordanceTitle}>Affordance</div>
-          </div>
-          <div className={style.descriptionCard}>
-            <div className={style.projectDescription}>Project Description</div>
-          </div>
+          <div className={style.childCard}>something</div>
+          <div className={style.exampleCard}>Picture in my background-image</div>
+          <div className={style.descriptionCard}>something</div>
         </div>
-        <div className="text-center">
+
+        <div className="generateButtonContainer" style={{ textAlign: "center", marginTop: "2rem" }}>
           <GenerateAttribution
             className={style.generatedButton}
             //onClick={this.toggleHiddenPassion.bind(this)}
           />
-          <Link className="button" to={this.props.nextRoute}>
+          <Link className="button" to={nextRoute}>
             Continue ➞
           </Link>
         </div>
-      </CardsContainer>
+      </Container>
     );
   }
 }
