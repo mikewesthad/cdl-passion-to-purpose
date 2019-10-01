f86d9f6212ac1c8142b7925ac6d5377fbe11d217
henryhoare <henry.hoare@hotmail.com>
2019-09-30 19:27:43 -0500

    js to pdf stuff

--- src/pages/journey-map/index.js
+++ src/pages/journey-map/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { observer, inject } from "mobx-react";
 import Container from "../../components/container";
 import style from "./index.module.scss";
@@ -7,12 +7,42 @@ import SocialShare from "../../components/social-share";
 import Attribution from "../../components/attribution";
 import FacilitatorAttribution from "../../components/facilitator-attribution";
 import Survey from "../../components/survey";
+import ReactDOM from "react-dom";
+import jsPDF from "jspdf";
+
+const noop = () => {};
 
 class JourneyMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = { combinationNumber: 0 };
+    this.elementRef = React.createRef();
   }
+
+  savePDF = () => {
+    // See https://rawgit.com/MrRio/jsPDF/master/docs/jsPDF.html
+    const doc = new jsPDF({
+      orientation: "p",
+      unit: "px",
+      format: "letter"
+    });
+    // HTML element to render, using React's API for accessing DOM elements:
+    // https://reactjs.org/docs/refs-and-the-dom.html
+    const element = this.elementRef.current;
+    const margins = {
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0
+    };
+    const options = {
+      width: 500
+    };
+    // See example usage from repo: https://github.com/MrRio/jsPDF/blob/b9f932cd2e22c82db0a39f0225521945a2568809/examples/basic.html#L357
+    // DOM element, x pos to render on page, y pos to render on page, callback, options
+    doc.fromHTML(element, margins.left, margins.right, options, noop, margins);
+    doc.save("hmw.pdf");
+  };
 
   getNextCombination = () => {
     this.setState(prev => {
@@ -25,10 +55,15 @@ class JourneyMap extends React.Component {
     const passion = gameData.passionStore.responses[gameData.chosenPassionIndex];
     const purpose = gameData.getPurposesWithVerb()[gameData.chosenPurposeIndex];
     return (
+      //<div className="pdfContainer" ref={this.elementRef}>
       <Container>
         <h1 className="title">Journey Map</h1>
         <hr />
-
+        {/* 
+          <div className="title" ref={this.elementRef}>
+            <button onClick={this.savePDF}>Generate PDF</button>
+          </div>
+*/}
         <div className={style.generatedQuestion}>
           <span className={style.bolded}>How might we use </span>
           <span className={style.generatedPassion}>{passion}</span>
@@ -138,8 +173,9 @@ class JourneyMap extends React.Component {
         <Survey style={{ marginTop: "2rem", textAlign: "center" }} />
         <Attribution style={{ marginTop: "2rem", textAlign: "center" }} />
       </Container>
+      //  </div>
     );
   }
 }
-
+const rootElement = document.getElementById("root");
 export default inject("gameData")(observer(JourneyMap));
