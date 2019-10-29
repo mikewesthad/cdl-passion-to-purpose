79c897d52cc648cd893bd26f04eb3c4bf812f87b
Lorelei <lorelei.miyamura@gmail.com>
2019-10-29 09:10:55 -0500

    Creating the initial timeline component

---
+++ src/components/timeline/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from "react";
+import style from "./index.module.scss";
+
+export default class Timeline extends PureComponent {
+  render() {
+    return (
+      <div className={style.timeline}>
+        <hr />
+      </div>
+    );
+  }
+}
