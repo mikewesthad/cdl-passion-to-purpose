c8373a498bbbab33e0d86dbae531df644482d0fa
Michael Hadley <michaelwesthadley@gmail.com>
2020-01-28 13:37:15 -0600

    Revert generate to non-animated commit (1f1269a08b64faa04d4a309c605d9e0c66293c2d)

--- src/pages/generator/index.js
+++ src/pages/generator/index.js
@@ -4,8 +4,6 @@ import { observer, inject } from "mobx-react";
 import GeneratorTemplate from "../../components/generator-template";
 import GenerateAttribution from "../../components/generator-attribution";
 import style from "./index.module.scss";
-import { TimelineLite } from "gsap/all";
-
 //
 class Generator extends React.Component {
   constructor(props) {
@@ -14,18 +12,6 @@ class Generator extends React.Component {
     const gameData = this.props.gameData;
     gameData.shufflePassions();
     gameData.shufflePurposes();
-
-    this.shuffleTl = new TimelineLite();
-    this.testRef = React.createRef();
-    this.passion1 = React.createRef();
-    this.passion2 = React.createRef();
-    this.passion3 = React.createRef();
-    this.passion4 = React.createRef();
-
-    this.purpose1 = React.createRef();
-    this.purpose2 = React.createRef();
-    this.purpose3 = React.createRef();
-    this.purpose4 = React.createRef();
   }
 
   toggleHiddenPassion() {
@@ -51,135 +37,51 @@ class Generator extends React.Component {
     console.log(this.render.purpose);
   };
 
-  componentDidMount() {
-    var speed = 0.75;
-    var linesize = 250;
-
-    this.shuffleTl
-      .to(this.passion1.current, 0, { y: -linesize * 2 }, 0)
-      .to(this.passion2.current, 0, { y: -linesize * 2 }, 0)
-      .to(this.passion3.current, 0, { y: -linesize * 2 }, 0)
-      .to(this.passion4.current, 0, { y: -linesize * 2 }, 0)
-
-      //First 'Spin' down
-      .to(this.passion1.current, speed, { y: linesize * 2 }, 0.5)
-      .to(this.passion2.current, speed, { y: linesize * 2 }, 0.75)
-      .to(this.passion3.current, speed, { y: linesize * 2 }, 1)
-      .to(this.passion4.current, speed, { y: linesize * 2 }, 1.25)
-
-      //Back to top
-      .to(this.passion1.current, 0, { y: -linesize * 2 }, 1.75)
-      .to(this.passion2.current, 0, { y: -linesize * 2 }, 1.75)
-      .to(this.passion3.current, 0, { y: -linesize * 2 }, 1.75)
-      .to(this.passion4.current, 0, { y: -linesize * 2 }, 1.75)
-
-      //Second 'Spin' down
-      .to(this.passion1.current, speed, { y: linesize * 2 }, 2)
-      .to(this.passion2.current, speed, { y: linesize * 2 }, 2.25)
-      .to(this.passion3.current, speed, { y: linesize * 2 }, 2.5)
-      .to(this.passion4.current, speed, { y: linesize * 2 }, 2.75)
-
-      //Back to top
-      .to(this.passion1.current, 0, { y: -linesize * 2 }, 3.25)
-      .to(this.passion2.current, 0, { y: -linesize * 2 }, 3.25)
-      .to(this.passion3.current, 0, { y: -linesize * 2 }, 3.25)
-      .to(this.passion4.current, 0, { y: -linesize * 2 }, 3.25)
-
-      //Third 'Spin' down
-      .to(this.passion1.current, speed, { y: linesize * 2 }, 3.5)
-      .to(this.passion2.current, speed, { y: linesize * 2 }, 3.75)
-      .to(this.passion3.current, speed, { y: linesize * 2 }, 4)
-      .to(this.passion4.current, speed, { y: linesize * 2 }, 4.25)
-
-      //Back to top
-      .to(this.passion1.current, 0, { y: -linesize * 2 }, 4.75)
-      .to(this.passion2.current, 0, { y: -linesize * 2 }, 4.75)
-      .to(this.passion3.current, 0, { y: -linesize * 2 }, 4.75)
-      .to(this.passion4.current, 0, { y: -linesize * 2 }, 4.75)
-
-      /////////////////////////// PURPOSE ////////////////////////////
-
-      .to(this.purpose1.current, 0, { y: -linesize * 3 }, 0)
-      .to(this.purpose2.current, 0, { y: -linesize * 3 }, 0)
-      .to(this.purpose3.current, 0, { y: -linesize * 3 }, 0)
-      .to(this.purpose4.current, 0, { y: -linesize * 3 }, 0)
-
-      //First 'Spin' down
-      .to(this.purpose1.current, speed, { y: linesize * 3 }, 0.5)
-      .to(this.purpose2.current, speed, { y: linesize * 3 }, 0.75)
-      .to(this.purpose3.current, speed, { y: linesize * 3 }, 1)
-      .to(this.purpose4.current, speed, { y: linesize * 3 }, 1.25)
-
-      //Back to top
-      .to(this.purpose1.current, 0, { y: -linesize * 3 }, 1.75)
-      .to(this.purpose2.current, 0, { y: -linesize * 3 }, 1.75)
-      .to(this.purpose3.current, 0, { y: -linesize * 3 }, 1.75)
-      .to(this.purpose4.current, 0, { y: -linesize * 3 }, 1.75)
-
-      //Second 'Spin' down
-      .to(this.purpose1.current, speed, { y: linesize * 3 }, 2)
-      .to(this.purpose2.current, speed, { y: linesize * 3 }, 2.25)
-      .to(this.purpose3.current, speed, { y: linesize * 3 }, 2.5)
-      .to(this.purpose4.current, speed, { y: linesize * 3 }, 2.75)
-
-      //Back to top
-      .to(this.purpose1.current, 0, { y: -linesize * 3 }, 3.25)
-      .to(this.purpose2.current, 0, { y: -linesize * 3 }, 3.25)
-      .to(this.purpose3.current, 0, { y: -linesize * 3 }, 3.25)
-      .to(this.purpose4.current, 0, { y: -linesize * 3 }, 3.25)
-
-      //Third 'Spin' down
-      .to(this.purpose1.current, speed, { y: linesize * 3 }, 3.5)
-      .to(this.purpose2.current, speed, { y: linesize * 3 }, 3.75)
-      .to(this.purpose3.current, speed, { y: linesize * 3 }, 4)
-      .to(this.purpose4.current, speed, { y: linesize * 3 }, 4.25)
-
-      //Back to top
-      .to(this.purpose1.current, 0, { y: -linesize * 3 }, 4.75)
-      .to(this.purpose2.current, 0, { y: -linesize * 3 }, 4.75)
-      .to(this.purpose3.current, 0, { y: -linesize * 3 }, 4.75)
-      .to(this.purpose4.current, 0, { y: -linesize * 3 }, 4.75)
-
-      .to(this.passion1.current, speed, { y: 0 }, 5)
-      .to(this.purpose1.current, speed, { y: 0 }, 5);
-  }
-
   render() {
     const { gameData, nextRoute } = this.props;
-    const passion1 = gameData.passionStore.responses[gameData.chosenPassionIndex];
-    const passion2 = gameData.passionStore.responses[gameData.chosenPassionIndex + 1];
-    const passion3 = gameData.passionStore.responses[gameData.chosenPassionIndex + 2];
-    const passion4 = gameData.passionStore.responses[gameData.chosenPassionIndex + 3];
-    const purpose1 = gameData.getPurposesWithVerb()[gameData.chosenPurposeIndex];
-    const purpose2 = gameData.getPurposesWithVerb()[gameData.chosenPurposeIndex + 1];
-    const purpose3 = gameData.getPurposesWithVerb()[gameData.chosenPurposeIndex + 2];
-    const purpose4 = gameData.getPurposesWithVerb()[gameData.chosenPurposeIndex + 3];
+    const passion = gameData.passionStore.responses[gameData.chosenPassionIndex];
+    const purpose = gameData.getPurposesWithVerb()[gameData.chosenPurposeIndex];
+    const PassionChild = () => <span className={style.generatedPassion}>{passion}</span>;
+    const PassionParent = () => (
+      <div className={style.overflowContainer}>
+        <div className={style.animation}>
+          <span className={style.generatedPassion}>
+            <ul>
+              <li>{gameData.passionStore.responses[0]}</li>
+              <li>{gameData.passionStore.responses[1]}</li>
+              <li>{gameData.passionStore.responses[2]}</li>
+              <li>{gameData.passionStore.responses[3]}</li>
+            </ul>
+          </span>
+        </div>
+      </div>
+    );
+    const PurposeChild = () => <span className={style.generatedPurpose}>{purpose}</span>;
+    const PurposeParent = () => (
+      <div className={style.overflowContainer}>
+        <div className={style.animation}>
+          <span className={style.generatedPurpose}>
+            <ul>
+              <li>{gameData.getPurposesWithVerb()[0]}</li>
+              <li>{gameData.getPurposesWithVerb()[1]}</li>
+              <li>{gameData.getPurposesWithVerb()[2]}</li>
+              <li>{gameData.getPurposesWithVerb()[3]}</li>
+            </ul>
+          </span>
+        </div>
+      </div>
+    );
 
     return (
       <GeneratorTemplate>
         <div className={style.generatedQuestion}>
-          <span className={style.hmwPreset} ref={this.testRef}>
-            How might we use{" "}
-          </span>
+          <span className={style.hmwPreset}>How might we use </span>
           <div className={style.passionContainer}>
             <GenerateAttribution
               className={style.generatedButton}
               onClick={this.toggleHiddenPassion.bind(this)}
             />
-            <div className={style.overflowContainer}>
-              <span className={style.generatedPassion} ref={this.passion1}>
-                {passion1}
-              </span>
-              <span className={style.generatedPassion} ref={this.passion2}>
-                {passion2}
-              </span>
-              <span className={style.generatedPassion} ref={this.passion3}>
-                {passion3}
-              </span>
-              <span className={style.generatedPassion} ref={this.passion4}>
-                {passion4}
-              </span>
-            </div>
+            <span className={style.generatedPassion}>{passion}</span>
           </div>
           <span className={style.hmwPreset}>to</span>
           <div className={style.purposeContainer}>
@@ -187,20 +89,8 @@ class Generator extends React.Component {
               className={style.generatedButton}
               onClick={this.toggleHiddenPurpose.bind(this)}
             />
-            <div className={style.overflowContainerPurpose}>
-              <span className={style.generatedPurpose} ref={this.purpose1}>
-                {purpose1}?
-              </span>
-              <span className={style.generatedPurpose} ref={this.purpose2}>
-                {purpose2}?
-              </span>
-              <span className={style.generatedPurpose} ref={this.purpose3}>
-                {purpose3}?
-              </span>
-              <span className={style.generatedPurpose} ref={this.purpose4}>
-                {purpose4}?
-              </span>
-            </div>
+
+            <span className={style.generatedPurpose}>{purpose}?</span>
           </div>
         </div>
         <div className="generateButtonContainer" style={{ textAlign: "center" }}>
