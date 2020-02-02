d00d28db6aecfcbc2faa4e7fa0a970c1ec34225f
loreleim <lorelei.miyamura@gmail.com>
2020-02-02 15:12:44 -0600

    temporarily putting everything onto a single .js component

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -1,13 +1,17 @@
 import React, { PureComponent } from "react";
 import style from "./index.module.scss";
-import TimelineFill from "../timeline/inner-timeline";
+//import TimelineFill from "../timeline/inner-timeline";
+
+const percentage = {
+  width: "20%"
+};
 
 export default class Timeline extends PureComponent {
   render() {
     return (
       <div className={style.timelineContainer}>
         <div className={style.timeline}>
-          {this.props.children} <TimelineFill />
+          {this.props.children} <div className={style.timelineFill} style={percentage} />
         </div>
       </div>
     );
