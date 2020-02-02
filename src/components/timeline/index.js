4da4f43bbdc0ecf62fefaada782100db29a6e82e
loreleim <lorelei.miyamura@gmail.com>
2020-02-02 13:56:40 -0600

    optimizing the timeline, no need for an outer component

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -1,17 +1,14 @@
 import React, { PureComponent } from "react";
 import style from "./index.module.scss";
-import OuterTimeline from "../timeline/outer-timeline";
 import TimelineFill from "../timeline/inner-timeline";
-
-const Chicken = <div>Chicken</div>;
 
 export default class Timeline extends PureComponent {
   render() {
     return (
       <div className={style.timelineContainer}>
-        <OuterTimeline>
-          <TimelineFill />
-        </OuterTimeline>
+        <div className={style.timeline}>
+          {this.props.children} <TimelineFill percentage={10} />
+        </div>
       </div>
     );
   }
