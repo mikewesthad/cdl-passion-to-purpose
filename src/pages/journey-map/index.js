d1432d614bbde128b7978291e8f086881e8af34c
henryhoare <henry.hoare@hotmail.com>
2020-02-02 14:13:03 -0600

    Commented out all "Let's Make" content in the journey map

--- src/pages/journey-map/index.js
+++ src/pages/journey-map/index.js
@@ -58,7 +58,7 @@ class JourneyMap extends React.Component {
       //<div className="pdfContainer" ref={this.elementRef}>
       <Container>
         <h1 className="title">Journey Map</h1>
-        <button classname={style.pdfButton} onClick={() => window.print()}>
+        <button className={style.pdfButton} onClick={() => window.print()}>
           Save as PDF
         </button>
         <hr />
@@ -73,7 +73,7 @@ class JourneyMap extends React.Component {
           <span className={style.bolded}> to </span>
           <span className={style.generatedPurpose}>{purpose}?</span>
         </div>
-
+        {/*
         <hr />
 
         <div className={style.generatedQuestion}>
@@ -84,7 +84,7 @@ class JourneyMap extends React.Component {
         </div>
 
         <hr />
-
+*/}
         <div className={style.generatedQuestion}>
           <span className={style.bolded}>You started off by pondering your passions.</span>
           <div className="description">
@@ -133,7 +133,7 @@ class JourneyMap extends React.Component {
           </div>
         </div>
         <hr />
-
+        {/*
         <div className={style.generatedQuestion}>
           <span className={style.bolded}>
             Next you began exploring ways to answer your “How Might We” question by choosing a
@@ -169,12 +169,15 @@ class JourneyMap extends React.Component {
           <span className={style.bolded}> to </span>
           <span className={style.generatedPurpose}>{gameData.impact}!</span>
         </div>
+
+        */}
+
         <span className={style.bolded}>Congratulations on your great work!</span>
         <hr />
         <form />
 
         <div className="text-center">
-          <SocialShare passion={gameData.passionStore.responses[gameData.chosenPassionIndex]} />
+          {/*    <SocialShare passion={gameData.passionStore.responses[gameData.chosenPassionIndex]} />*/}
         </div>
         <FacilitatorAttribution style={{ marginTop: "2rem", textAlign: "center" }} />
         <Survey style={{ marginTop: "2rem", textAlign: "center" }} />
