fc8c8f0d3206c20bf66cf24da692de251852b9c4
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-02-04 12:58:52 -0600

    moving over the old "filler" child component to just be in the main component

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -5,7 +5,10 @@ export default class Timeline extends PureComponent {
   render() {
     return (
       <div className={style.timelineContainer}>
-        <div className={style.timeline}>{this.props.children}</div>
+        <div className={style.timeline}>
+          {this.props.children}
+          <div className={style.timelineFill} style={this.props.testing} />
+        </div>
       </div>
     );
   }
