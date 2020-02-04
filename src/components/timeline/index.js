003e9ce662bc51a358f6223cfa41bc71c14ea1f6
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-02-04 10:19:29 -0600

    removing all the testing & old code

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -2,20 +2,10 @@ import React, { PureComponent } from "react";
 import style from "./index.module.scss";
 //import TimelineFill from "../timeline/inner-timeline";
 
-const percentage = {
-  width: "50%"
-};
-
 export default class Timeline extends PureComponent {
   render() {
-    console.log(this.props);
-    //const test = this.props; /* is this what i want to do ... idk */
     return (
       <div className={style.timelineContainer}>
-        {/*{this.props.testing}*/}
-        <div className={style.timeline}>
-          {this.props.children} <div className={style.timelineFill} style={this.props.testing} />
-        </div>
       </div>
     );
   }
--- src/components/timeline/inner-timeline.js
+++ src/components/timeline/inner-timeline.js
@@ -1,12 +1,7 @@
 import React, { PureComponent } from "react";
 import style from "./index.module.scss";
 
-const percentage = {
-  width: "50%"
-};
-
 export default class Fill extends PureComponent {
   render() {
-    return <div className={style.timelineFill} style={percentage} />;
   }
 }
