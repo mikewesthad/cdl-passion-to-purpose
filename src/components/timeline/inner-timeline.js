4f025d29713feb5d00e36524495dd979e9d5563f
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-02-04 15:31:49 -0600

    removing the old filler timeline component

--- src/components/timeline/inner-timeline.js
+++
@@ -1,8 +0,0 @@
-import React, { PureComponent } from "react";
-import style from "./index.module.scss";
-
-export default class Fill extends PureComponent {
-  render() {
-    return <div className={style.timelineFill} style={this.props.testing} />;
-  }
-}
