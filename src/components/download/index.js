1c75551ae993ba360e825536bc5ffede36d323ad
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-02-18 12:41:47 -0600

    download button component

---
+++ src/components/download/index.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from "react";
+import Button from "../nav/button";
+import { ReactComponent as DownloadSvg } from "../../images/download.svg"; //previously restart.svg
+import style from "./index.module.scss";
+import JourneyMap from "../journey-map";
+
+export default class Nav extends PureComponent {
+  printOrder = () => {
+    const something = "wow";
+    const oldPage = document.body.innerHTML;
+    document.body.innerHTML = something;
+    window.print();
+    document.body.innerHTML = oldPage;
+  };
+  render() {
+    const { disabled = false, onBack } = this.props;
+    return (
+      <div className={style.downloadContainer}>
+        <Button className={style.download} disabled={disabled} onClick={() => this.printOrder()}>
+          <DownloadSvg />
+        </Button>
+      </div>
+    );
+  }
+}
