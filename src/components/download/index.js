d7a7311f3f25390dd248108b269808930515a991
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-02-24 17:15:46 -0600

    now data is being pulled in prior to the render

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -1,21 +1,30 @@
 import React, { PureComponent } from "react";
 import Button from "../nav/button";
+import { observer, inject } from "mobx-react";
 import { ReactComponent as DownloadSvg } from "../../images/download.svg"; //previously restart.svg
 import style from "./index.module.scss";
 import jsPDF from "jspdf";
 
-export default class Nav extends PureComponent {
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    const gameData = this.props.gameData;
+  }
+
   savePDF = () => {
     const doc = new jsPDF({
       orientation: "p",
       unit: "px",
       format: "letter"
     });
-    doc.fromHTML("chicken");
+    const testing = "" + this.props.gameData.passionStore.responses[0];
+    doc.fromHTML(testing + "chicken");
     doc.save("hmw.pdf");
   };
+
   render() {
-    const { disabled = false, onBack } = this.props;
+    const { disabled = false, onBack, gameData } = this.props;
+    console.log(this.props.gameData.passionStore.responses[0]);
     return (
       <div className={style.downloadContainer}>
         <Button className={style.download} disabled={disabled} onClick={() => this.savePDF()}>
@@ -25,3 +34,5 @@ export default class Nav extends PureComponent {
     );
   }
 }
+
+export default inject("gameData")(observer(Nav));
