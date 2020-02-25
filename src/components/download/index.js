c7d991cb4b4942342f09397f5bc07b5614460080
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-02-24 21:46:17 -0600

    working download button pdf

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -8,7 +8,7 @@ import jsPDF from "jspdf";
 class Nav extends React.Component {
   constructor(props) {
     super(props);
-    const gameData = this.props.gameData;
+    this.userResponses = React.createRef();
   }
 
   savePDF = () => {
@@ -17,19 +17,73 @@ class Nav extends React.Component {
       unit: "px",
       format: "letter"
     });
-    const testing = "" + this.props.gameData.passionStore.responses[0];
-    doc.fromHTML(testing + "chicken");
+    const journeyMap = this.userResponses.current;
+    doc.fromHTML(journeyMap);
+    doc.addPage();
     doc.save("hmw.pdf");
   };
 
   render() {
-    const { disabled = false, onBack, gameData } = this.props;
-    console.log(this.props.gameData.passionStore.responses[0]);
+    const { disabled = false, gameData } = this.props;
+
     return (
       <div className={style.downloadContainer}>
         <Button className={style.download} disabled={disabled} onClick={() => this.savePDF()}>
           <DownloadSvg />
         </Button>
+        <div className={style.hidden} ref={this.userResponses}>
+          <div className={style.generatedQuestion}>
+            <span className={style.bolded}>How might we use </span>
+            <span className={style.generatedPassion}>
+              {gameData.passionStore.responses[gameData.chosenPassionIndex]}
+            </span>
+            <span className={style.bolded}> to </span>
+            <span className={style.generatedPurpose}>
+              {gameData.getPurposesWithVerb()[gameData.chosenPurposeIndex]}?
+            </span>
+          </div>
+          <div className={style.generatedQuestion}>
+            <h1>You started off by pondering your passions.</h1>
+            <div className="description">
+              <div className={style.centerRight}>
+                1. {gameData.passionStore.questions[0].replace("...", " ")}{" "}
+                {gameData.passionStore.responses[0]}.<br />
+                2. {gameData.passionStore.questions[1].replace("...", " ")}{" "}
+                {gameData.passionStore.responses[1]}.<br />
+                3. {gameData.passionStore.questions[2].replace("...", " ")}{" "}
+                {gameData.passionStore.responses[2]}.<br />
+                4. {gameData.passionStore.questions[3].replace("...", " ")}
+                {gameData.passionStore.responses[3]}.
+              </div>
+            </div>
+          </div>
+          <div className={style.generatedQuestion}>
+            <h1>Then you took a look at your purpose.</h1>
+            <div className="description">
+              <div className={style.centerRight}>
+                1. I want to {gameData.getPurposesWithVerb()[0]}.<br />
+                2. I want to {gameData.getPurposesWithVerb()[1]}.<br />
+                3. I want to {gameData.getPurposesWithVerb()[2]}.<br />
+                4. I want to {gameData.getPurposesWithVerb()[3]}.
+              </div>
+            </div>
+          </div>
+          <div className={style.generatedQuestion}>
+            Next, you juxtaposed your passions and purposes <br />
+            Combining them into a "How Might We" design question. <br />
+            Your selected HMW question shown below: <br />
+            <div className={style.generatedQuestion}>
+              <span className={style.bolded}>How might we use </span>
+              <span className={style.generatedPassion}>
+                {gameData.passionStore.responses[gameData.chosenPassionIndex]}
+              </span>
+              <span className={style.bolded}> to </span>
+              <span className={style.generatedPurpose}>
+                {gameData.getPurposesWithVerb()[gameData.chosenPurposeIndex]}?
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
