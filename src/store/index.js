41a13d599b8cfce0790b605271b17d840f992444
henryhoare <henry.hoare@hotmail.com>
2020-03-31 17:51:16 -0400

    Disabled auto input for responses

--- src/store/index.js
+++ src/store/index.js
@@ -553,8 +553,8 @@ class ResponsesStore {
 const store = new GameData();
 
 //Testing;
-
+/*
 store.passionStore.setResponses(["food", "Lego", "video games", "beyonce"]);
 store.purposeStore.setResponses(["equality", "rats", "poverty in America", "testing"]);
-
+*/
 export default store;
