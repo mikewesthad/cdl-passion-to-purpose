52db1b78971aa776071076633ab64a961c46a27c
henryhoare <henry.hoare@hotmail.com>
2020-04-06 12:52:28 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.abccaa641b89cbf851966323d8617ca1.js"
+  "./precache-manifest.c47b17b452c9aa9df91a73ae6943dc43.js"
 );
 
 workbox.clientsClaim();
